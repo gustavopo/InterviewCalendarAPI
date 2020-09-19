1b38e97eec69e4b6e7e1348be728cd0b9b2de121
Gustavo\xpand <gustavo.oliveira@xpand-it.com>
2020-09-19 12:24:36 +0100

    First commit

---
+++ src/main/java/tamanna/challange/interviews/InterviewsSchedulerApplication.java
@@ -0,0 +1,13 @@
+package tamanna.challange.interviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewsSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewsSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tamanna/challange/interviews/InterviewsSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package tamanna.challange.interviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewsSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
