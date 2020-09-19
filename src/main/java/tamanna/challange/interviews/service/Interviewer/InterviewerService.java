c1d3f4782ecd65527bc4209d661433b92f11da9d
Gustavo\xpand <gustavo.oliveira@xpand-it.com>
2020-09-19 16:27:36 +0100

    Interviewer Resource (Controller, Service and Repository)

---
+++ src/main/java/tamanna/challange/interviews/service/Interviewer/InterviewerService.java
@@ -0,0 +1,30 @@
+package tamanna.challange.interviews.service.Interviewer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tamanna.challange.interviews.model.Interviewer;
+import tamanna.challange.interviews.model.Slot;
+import tamanna.challange.interviews.repository.InterviewerRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class InterviewerService implements IInterviewerService{
+
+    @Autowired
+    private InterviewerRepository interviewerRepository;
+
+    @Override
+    public List<Interviewer> getAll() {
+        return interviewerRepository.findAll();
+    }
+
+    @Override
+    public Optional<Interviewer> getById(Integer interviewerId) {
+        return interviewerRepository.getById(interviewerId);
+    }
+
+    @Override
+    public List<Slot> getAvailableSlots(Interviewer interviewer) {
+        return interviewerRepository.getSlotsByInterviewer(interviewer);
+    }
+}
