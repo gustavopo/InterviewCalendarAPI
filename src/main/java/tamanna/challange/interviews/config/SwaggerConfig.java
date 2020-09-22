37f30b65b103dddb3012f9aa6bcba89f86652397
Gustavo\xpand <gustavo.oliveira@xpand-it.com>
2020-09-22 01:40:50 +0100

    Maven Dependencies and Swagger Configuration File

---
+++ src/main/java/tamanna/challange/interviews/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package tamanna.challange.interviews.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                    .apiInfo(apiInfo());
+        }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Interview Scheduler",
+                "Application to schedule interviews",
+                "1.0.0",
+                "",
+                new Contact("", "", ""),
+                "", "", Collections.emptyList());
+
+    }
+    }
