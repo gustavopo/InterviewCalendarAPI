3d3a1c59faf4a4a88cee4ee3ed05e92de9c292f6
Gustavo\xpand <gustavo.oliveira@xpand-it.com>
2020-09-22 01:56:39 +0100

    Controller Documentation

--- src/main/java/tamanna/challange/interviews/controller/CandidateController.java
+++ src/main/java/tamanna/challange/interviews/controller/CandidateController.java
@@ -1,7 +1,10 @@
 package tamanna.challange.interviews.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import tamanna.challange.interviews.model.Person.Candidate;
 import tamanna.challange.interviews.model.Schedule.RequestedInterviewDates;
@@ -12,31 +15,32 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(description = "Candidate Endpoint", value = "CandidateController" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class CandidateController {
     @Autowired
     ICandidateService iCandidateService;
 
-    public void setiCandidateService(ICandidateService iCandidateService) {
-        this.iCandidateService = iCandidateService;
-    }
-
     @GetMapping("/api/candidates")
+    @ApiOperation(value = "Get all Candidates")
     public List<Candidate> getCandidates(){
         return iCandidateService.getAll();
     }
 
     @GetMapping("/api/candidates/{candidateId}")
+    @ApiOperation(value = "Get a Candidate by Id")
     public Optional<Candidate> getCandidateById(@PathVariable(name="candidateId") Integer candidateId){
         return iCandidateService.getCandidateById(candidateId);
     }
 
     @PostMapping("/api/candidates")
+    @ApiOperation(value = "Insert a new Candidate")
     public void insertCandidate(Candidate candidates){
         iCandidateService.insertCandidates(candidates);
         System.out.println("Candidate Saved Successfully");
     }
 
     @PostMapping("/api/candidates/{candidateId}/requestDates")
+    @ApiOperation(value = "Set Candidate Request Interview Dates")
     public void setCandidateRequestDates(@PathVariable(name="candidateId") Integer candidateId,
                                         @RequestParam("requestDate")
                                         @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm") List<LocalDateTime> requestDates){
@@ -44,6 +48,7 @@ public class CandidateController {
     }
 
     @GetMapping("/api/candidates/{candidateId}/requestedDates")
+    @ApiOperation(value = "Get requested Interviews by Candidate ID")
     public List<RequestedInterviewDates> getRequestedInterviewDatesByCandidate (@PathVariable(name="candidateId") Integer candidateId){
         return iCandidateService.getRequestedDatesByCandidate(candidateId);
     }
--- src/main/java/tamanna/challange/interviews/controller/InterviewerController.java
+++ src/main/java/tamanna/challange/interviews/controller/InterviewerController.java
@@ -1,7 +1,10 @@
 package tamanna.challange.interviews.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import tamanna.challange.interviews.model.Schedule.AvailableInterviewDates;
 import tamanna.challange.interviews.model.Person.Interviewer;
@@ -11,30 +14,32 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
+@Api(description = "Interviewer Endpoint", value = "InterviewerController" , produces = MediaType.APPLICATION_JSON_VALUE)
 public class InterviewerController {
     @Autowired
     IInterviewerService iInterviewerService;
 
-    public void setiInterviewerService(IInterviewerService iInterviewerService) {
-        this.iInterviewerService = iInterviewerService;
-    }
     @GetMapping("/api/interviewers")
+    @ApiOperation(value = "Get all Interviewers")
     public List<Interviewer> getInterviewers(){
         return iInterviewerService.getAll();
     }
 
     @GetMapping("/api/interviewers/{interviewerId}")
+    @ApiOperation(value = "Get an Interviewer by Id")
     public Interviewer getInterviewerById(@PathVariable(name="interviewerId") Integer interviewerId){
         return iInterviewerService.getInterviewerById(interviewerId).get();
     }
 
     @PostMapping("/api/interviewers")
+    @ApiOperation(value = "Insert new Interviewer")
     public void insertInterviewer(Interviewer interviewers){
         iInterviewerService.insertInterviewer(interviewers);
         System.out.println("Interviewer Saved Successfully");
     }
 
     @PostMapping("/api/interviewers/{interviewerId}/setAvailableDates")
+    @ApiOperation(value = "Set Interviewer Available Interview Dates")
     public void setInterviewerAvailableDates(@PathVariable(name="interviewerId") Integer interviewerId,
                                    @RequestParam("availableDates")
                                    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm") List<LocalDateTime> availableDates){
@@ -43,6 +48,7 @@ public class InterviewerController {
     }
 
     @GetMapping("/api/interviewers/{interviewerId}/availableDates")
+    @ApiOperation(value = "Get available Interviews by Interviewer ID")
     public List<AvailableInterviewDates> getAvailableDatesByInterviewer (@PathVariable(name="interviewerId") Integer interviewerId){
         return iInterviewerService.getAvailableDatesByInterviewer(interviewerId);
     }
